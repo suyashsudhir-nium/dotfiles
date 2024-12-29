9832e959edc8f5f68000bc3fbff7791a2eb93bd5
Suyash Sudhir <suyash.sudhir@nium.com>
2024-12-29 17:10:20 +0530

    add translucency to ghostty

--- nvim/lua/plugins/github-theme.lua
+++ nvim/lua/plugins/github-theme.lua
@@ -4,6 +4,12 @@ return {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd('colorscheme github_dark_default')
-  end
+    require('github-theme').setup({
+      options = {
+        transparent = true
+      }
+    })
+
+    vim.cmd('colorscheme github_dark')
+  end,
 }
