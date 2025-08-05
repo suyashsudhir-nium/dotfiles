b17b530480f52f8773fd5e621bcd4d7810047456
Suyash Sudhir <suyash.sudhir@nium.com>
2025-08-05 11:15:19 +0530

    backup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
